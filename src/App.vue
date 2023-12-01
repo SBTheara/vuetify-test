b639791a8ea2f5bdc441568b1e3852791ac98682
ALLWEB\sambathth <theara.sambath@allweb.com.kh>
2023-12-01 16:34:58 +0700

    new config

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,109 @@
 <template>
-  <v-app>
+  <v-app class="grey darken-4">
+    <v-app-bar class="grey darken-3 align-end" app height="100px" width="100%">
+        <v-app-bar-nav-icon @click="drawer = !drawer" plain class="grey--text text--lighten-3"></v-app-bar-nav-icon>
+        <v-toolbar-title class="font-weight-bold">
+          <span class="grey--text text--lighten-3">Movie</span>
+          <span class="red--text text-darken-3">App</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          class="mt-6"
+          outlined
+          rounded
+          append-icon="mdi-magnify"
+          dense
+        >
+      </v-text-field>
+      <v-spacer></v-spacer>
+        <v-btn text plain><v-icon>mdi-share</v-icon></v-btn>
+    </v-app-bar>
+    <v-navigation-drawer
+          class="grey darken-3"
+          v-model="drawer"
+          app
+        >
+        <v-list-item >
+          <v-list-item-content>
+            <v-list-item-title>
+              <span class="text-h6 grey--text text--lighten-3 font-weight-bold">Movie</span>
+              <span class="text-h6 red--text font-weight-bold">App</span>
+            </v-list-item-title>
+            <v-list-item-subtitle class="grey--text text--lighten-3 font-weight-bold">
+              The best website movie in the pagoda
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+      <v-divider></v-divider>
+        <v-list
+          dense
+          nav
+        >
+          <v-list-item
+            v-for="item in navItems"
+            :key="item.title"
+            link
+            class="grey--text text--lighten-3 font-weight-bold"
+            router :to="item.to"
+          >
+            <v-list-item-icon>
+              <v-icon class="grey--text text--lighten-3 font-weight-bold">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     <v-main>
-      <router-view/>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
+  export default { 
+    name: 'App',
+    data: () => ({ 
+      drawer: null ,
+      items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+      ],
+      cards: [
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+      
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+      
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+      
+      ],
+      navItems: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard',to: "/about" },
+          { title: 'Photos', icon: 'mdi-image',to: "/" },
+          { title: 'About', icon: 'mdi-help-box',to: "/" },
+      ],
+    
+    }),
+  }
 
-  data: () => ({
-    //
-  }),
-};
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,87 +1,60 @@
 <template>
-  <v-app class="grey darken-4">
-    <v-app-bar class="align-end" absolute height="100px" width="100%">
-        <v-app-bar-nav-icon @click="drawer = !drawer" plain class="grey--text text--lighten-3"></v-app-bar-nav-icon>
-        <v-toolbar-title class="font-weight-bold">
-          <span class="grey--text text--darken-3">Movie</span>
-          <span class="red--text text-darken-3">App</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-text-field
-          class="mt-6"
-          outlined
-          rounded
-          append-icon="mdi-magnify"
-          dense
-        >
-      </v-text-field>
-      <v-spacer></v-spacer>
-        <v-btn flat plain><v-icon>mdi-share</v-icon></v-btn>
-    </v-app-bar>
-    <v-navigation-drawer
-          v-model="drawer"
-          app
-        >
-        </v-navigation-drawer>
-    <v-main>
-      <v-carousel class="px-14" hide-delimiters>
-        <v-carousel-item
-          v-for="(item,i) in cards"
-          :key="i"
-          :src="item.src"
-        ></v-carousel-item>
-      </v-carousel>
-      <v-spacer></v-spacer>
-      <v-container>
-        <v-row>
-          <v-col
-            v-for="card in cards"
-            :key="card"
-            cols="4"
-          >
-            <v-card height="200"><v-img :src="card.src"></v-img></v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <div class="home">
+    <v-carousel hide-delimiters>
+      <v-carousel-item v-for="(item, i) in cards" :key="i" :src="item.src"></v-carousel-item>
+    </v-carousel>
+    <v-spacer></v-spacer>
+    <v-container>
+      <v-card flat>
+        <v-layout row my-3 wrap class="d-flex align-center pa-5">
+          <v-flex xs12 md6 v-for="(item,i) in items" :key="i">
+            <div>
+              <v-img
+                :src="item.src"
+                position="center"
+                cover>
+              </v-img>
+            </div>
+          </v-flex>
+          <!-- <v-flex xs12 md6>
+            <div>
+              <v-img :src="item.src"
+                position="center" contain>
+              </v-img>
+            </div>
+          </v-flex> -->
+          <v-divider></v-divider>
+        </v-layout>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
 
-  export default {
-    data: () => ({ 
-      drawer: null ,
-      items: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
-      ],
-      cards: [
-        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
-      
-        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
-      
-        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
-      
-      ],
-    
-    }),
-  }
+export default {
+  name: 'HomeView',
+  data: () => ({
+    cards: [
+      { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+
+      { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+
+      { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+
+    ],
+    items: [
+      {title:"My bike",src: 'https://myer-media.com.au/wcsstore/MyerCatalogAssetStore/images/77/776/7855/1/1/959274370/959274370_2_720x928.webp'},
+      {title:"My bike",src: 'https://www.voltes.eu/cdn/shop/files/bird-bike-stealth-black-1.png?v=1687364572'},
+      {title:"My bike",src: 'https://www.fujibikes.com/cdn/shop/products/2022_FUJI_JARI_11_MATTEBLACK_FRONT.png?v=1670262316&width=580'},
+    ]
+  }),
+}
 
 </script>
